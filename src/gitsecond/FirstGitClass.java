6bb023f6b263551c727902a0268b5f1056be2b28
babrak <babrak_m@hotmail.com>
2023-08-16 16:26:09 -0400

    NewProject

--- src/gitsecond/FirstGitClass.java
+++ src/gitsecond/FirstGitClass.java
@@ -5,6 +5,7 @@ public class FirstGitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is first branch changes");
+   System.out.println("Hello");
 	}
 
 }
