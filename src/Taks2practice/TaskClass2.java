af19e08243f1b6ae4c500196dfb2e0bfe8bb66ff
babrak <babrak_m@hotmail.com>
2023-08-16 09:23:02 -0400

    New Changes

--- src/Taks2practice/TaskClass2.java
+++ src/Taks2practice/TaskClass2.java
@@ -5,7 +5,9 @@ public class TaskClass2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is new changes from UpdtateToClass");
-
+          for (int i =0;i< 10;i++) {
+        	  System.out.println(i);
+          }
 	}
 
 }
